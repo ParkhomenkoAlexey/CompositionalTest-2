bdfe366a4689de69d543539fd229f54d39ee871a
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-06-20 15:19:19 +0500

    Initial Commit

---
+++ CompositionalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompositionalTest
+//
+//  Created by Алексей Пархоменко on 20.06.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
